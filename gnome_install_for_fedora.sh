cfd730a0139d083da238a8c09f780a9e5625592d
Simon <46846093+6im0n@users.noreply.github.com>
2022-10-17 13:45:58 +0200

    :sparkles:

---
+++ gnome_install_for_fedora.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+
+echo -e "\e[31m        _____________________________________"
+echo -e "\e[31m        |             \e[33m  Gnome             \e[31m  |"
+echo -e "\e[31m        |            \e[33m  Install          \e[31m    |"
+echo -e "\e[31m        |            \e[33m   Script           \e[31m   |"
+echo -e "\e[31m        |              \e[33m  for            \e[31m    |"
+echo -e "\e[31m        |             \e[33m  XCFE             \e[31m   |"
+echo -e "\e[31m        |___________________________________|"
+echo -e '\033[0m'
+sleep 3s
+sudo dnf update -y
+sudo dnf upgrade -y
+sudo systemctl disable lightdm
+sudo dnf install gdm
+sudo dnf install @gnome-desktop
+echo -e "\e[32m        _____________________________________"
+echo -e "\e[32m        |             \e[33m                    \e[32m  |"
+echo -e "\e[32m        |              \e[33m                 \e[32m    |"
+echo -e "\e[32m        |              \e[33m                 \e[32m    |"
+echo -e "\e[32m        |            \e[33m  Install          \e[32m    |"
+echo -e "\e[32m        |            \e[33m  Success!!         \e[32m   |"
+echo -e "\e[32m        |              \e[33m                 \e[32m    |"
+echo -e "\e[32m        |  \e[33m    reboot in... 15 seconds  \e[32m    |"
+echo -e "\e[32m        |           \e[33m                    \e[32m    |"
+echo -e "\e[32m        |            \e[33m                    \e[32m   |"
+echo -e "\e[32m        |___________________________________|"
+echo -e '\033[0m'
+sleep 15s
+sudo reboot now 
